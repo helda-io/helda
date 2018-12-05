4925137957d5673b0fa12b792d1d3b25d3a51321
legkvla <vladimir.legkunets@gmail.com>
2018-12-05 09:11:12 +0300

    Updating schema for listeners

--- src/helda/schema.clj
+++ src/helda/schema.clj
@@ -4,29 +4,14 @@
     )
   )
 
-(s/defschema Relation {
-    :relation-type s/Keyword
-    :src-attr s/Keyword
-    :dst-model s/Keyword
-    :dst-attr s/Keyword
-  })
-
 (s/defschema Model {
     :full-name s/Keyword
     :package s/Keyword
     :name s/Keyword
     (s/optional-key :description) s/Str
     :extends [s/Keyword]
-    :relaions [Relation]
-    :attrs {s/Keyword s/Keyword}
-  })
-
-(s/defschema Listener {
-    :id s/Str
-    :action-url s/Str
-    :world s/Keyword
-    :type s/Keyword
-    :obj-id s/Str
+    :attrs {s/Keyword s/Str} ;{attr schema}
+    :actions {s/Keyword s/Keyword} ;{action model}
   })
 
 (s/defschema World {
@@ -40,12 +25,20 @@
     :model s/Keyword
     :tags [s/Keyword]
     :attrs {s/Keyword s/Any}
+    :parent-id s/Str
+  })
+
+(s/defschema EntityListener {
+    :action-url s/Str
+    :world s/Keyword
+    :action s/Keyword
+    :entity-id s/Str
   })
 
 (s/defschema Event {
     :id s/Str
-    :listener-type s/Keyword
-    :world s/Keyword
-    :model s/Str
-    :instance Object
+    :action s/Keyword
+    :action-entity-id s/Str
+    :listener-id s/Str
+    :instance Entity
   })
